d968983e084ac047cd1cc686436434c44bbec149
Roma <rkurenok99@gmail.com>
2023-03-08 12:38:42 +0300

    added decide for tasks in capture 11.2

---
+++ learnjs-tasks/11. Промисы, async await/11.2. Промисы/11.2.3. Анимация круга с помощью промиса.js
@@ -0,0 +1,34 @@
+"use strict"
+
+// Перепишите функцию showCircle, написанную в задании Анимация круга с помощью колбэка таким образом, 
+// чтобы она возвращала промис, вместо того чтобы принимать в аргументы функцию-callback.
+
+function go() {
+    showCircle(150, 150, 100).then(div => {
+        div.classList.add('message-ball');
+        div.append("Hello, world!");
+    });
+}
+
+function showCircle(cx, cy, radius, callback) {
+    let div = document.createElement('div');
+    div.style.width = 0;
+    div.style.height = 0;
+    div.style.left = cx + 'px';
+    div.style.top = cy + 'px';
+    div.className = 'circle';
+    document.body.append(div);
+
+    return new Promise(resolve => {
+        setTimeout(() => {
+            div.style.width = radius * 2 + 'px';
+            div.style.height = radius * 2 + 'px';
+    
+            div.addEventListener('transitionend', function handler() {
+                div.removeEventListener('transitionend', handler);
+                resolve(div);
+            });
+        }, 0);
+    });
+}
+
